81de052b3bbb791f958ac4bfe975dd7f8623b943
rufdimitri <ruf.dimitri.de@gmail.com>
2023-11-22 09:23:48 +0100

    create project

---
+++ src/main/java/rd/pdfsearch/App.java
@@ -0,0 +1,13 @@
+package rd.pdfsearch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ start_pdfSearch.bat
@@ -0,0 +1,1 @@
+java -jar target/pdfSearch2-1.0-SNAPSHOT.jar
