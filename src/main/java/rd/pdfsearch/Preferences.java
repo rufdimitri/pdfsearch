d605cc89d8e6d39fb09033dcbc180c43be080a67
rufdimitri <ruf.dimitri.de@gmail.com>
2023-12-18 12:08:56 +0100

    add Preferences class

---
+++ src/main/java/rd/pdfsearch/Preferences.java
@@ -0,0 +1,31 @@
+package rd.pdfsearch;
+
+public class Preferences {
+    private String searchLocation;
+    private String keywords;
+    private String keywordsSeparator;
+
+    public String getSearchLocation() {
+        return searchLocation;
+    }
+
+    public void setSearchLocation(String searchLocation) {
+        this.searchLocation = searchLocation;
+    }
+
+    public String getKeywords() {
+        return keywords;
+    }
+
+    public void setKeywords(String keywords) {
+        this.keywords = keywords;
+    }
+
+    public String getKeywordsSeparator() {
+        return keywordsSeparator;
+    }
+
+    public void setKeywordsSeparator(String keywordsSeparator) {
+        this.keywordsSeparator = keywordsSeparator;
+    }
+}
