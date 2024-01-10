633033c9acdda06a813b7b69005059a8566872c8
rufdimitri <ruf.dimitri.de@gmail.com>
2024-01-10 12:00:24 +0100

    update JsonUtilTest: add test cases for Generic types

--- src/test/java/rd/pdfsearch/JsonUtilTest.java
+++ src/test/java/rd/pdfsearch/JsonUtilTest.java
@@ -1,10 +1,12 @@
 package rd.pdfsearch;
 
+import com.google.gson.reflect.TypeToken;
 import junit.framework.TestCase;
+import rd.pdfsearch.model.CachedPdfFile;
+import rd.pdfsearch.model.FileIdentity;
 import rd.util.JsonUtil;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class JsonUtilTest extends TestCase {
 
@@ -55,20 +57,51 @@ public class JsonUtilTest extends TestCase {
     public void testMarshallUnmarshall() {
         String filename = "target/file.json";
 
-        JsonUtil<TestPOJO> util = JsonUtil.of(TestPOJO.class);
         TestPOJO testObject1 = new TestPOJO();
         testObject1.setAge(33);
         testObject1.setName("ABC");
         testObject1.setVorname("def");
         testObject1.setAddress(Arrays.asList("street 1", "house 2"));
-        util.marshallToFile(filename, testObject1);
+        JsonUtil.marshallToFile(filename, testObject1);
 
-        JsonUtil<TestPOJO> util2 = JsonUtil.of(TestPOJO.class);
-
-        TestPOJO testObject1Recovered = util2.unmarshallFromFile(filename);
+        TestPOJO testObject1Recovered = JsonUtil.unmarshallFromFile(filename, new TypeToken<>() {
+        });
         assertEquals(33, testObject1Recovered.getAge());
         assertEquals("ABC", testObject1Recovered.getName());
         assertEquals("def", testObject1Recovered.getVorname());
         assertEquals(Arrays.asList("street 1", "house 2"), testObject1Recovered.getAddress());
+
+        //simpleMap
+        //create object and marshall
+        Map<String, Integer> initialSimpleMapObject = simpleMap();
+        String simpleMapStringIntegerJsonString = JsonUtil.marshallToJson(initialSimpleMapObject);
+        //unmarshall
+        Map<String, Integer> simpleMapStringIntegerObject = JsonUtil.unmarshallFromJson(simpleMapStringIntegerJsonString, new TypeToken<>() {
+        });
+        assertTrue(initialSimpleMapObject.equals(simpleMapStringIntegerObject));
+
+        //create object and marshall
+        Map<Integer, List<CachedPdfFile>> initialComplexMapObject = cachedFilesPerFileIdentityHashCode();
+        String complexMapJsonString = JsonUtil.marshallToJson(initialComplexMapObject);
+        //unmarshall
+        Map<Integer, List<CachedPdfFile>> complexMapObject = JsonUtil.unmarshallFromJson(complexMapJsonString, new TypeToken<>() {
+        });
+        assertTrue(initialComplexMapObject.equals(complexMapObject));
+    }
+
+    static Map<String,Integer> simpleMap() {
+        Map<String,Integer> map = new HashMap<>();
+        map.put("Abc", 3);
+        map.put("Abcd", 4);
+        map.put("Abcde", 5);
+        return map;
+    }
+
+    Map<Integer, List<CachedPdfFile>> cachedFilesPerFileIdentityHashCode() {
+        Map<Integer, List<CachedPdfFile>> result = new HashMap<>();
+        FileIdentity fileIdentity = new FileIdentity("file.txt", "folder1", 256L, 512L);
+        CachedPdfFile cachedPdfFile = new CachedPdfFile(List.of("page1", "page2"), fileIdentity, 559L);
+        result.put(777, List.of(cachedPdfFile, cachedPdfFile));
+        return result;
     }
 }
