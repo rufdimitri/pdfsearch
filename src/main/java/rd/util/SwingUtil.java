c43b1101fcbf1e38b8f202e96884cc1c9771395c
rufdimitri <ruf.dimitri.de@gmail.com>
2024-01-16 13:21:52 +0100

    fix format

--- src/main/java/rd/util/SwingUtil.java
+++ src/main/java/rd/util/SwingUtil.java
@@ -4,18 +4,18 @@ import java.awt.*;
 
 public class SwingUtil {
 
-    public static void changeFontRecursive(Component rootComponent, Font font ) {
-        rootComponent.setFont ( font );
-        if ( rootComponent instanceof Container ) {
-            for ( Component child : ( ( Container ) rootComponent ).getComponents () ) {
-                changeFontRecursive( child, font );
+    public static void changeFontRecursive(Component rootComponent, Font font) {
+        rootComponent.setFont(font);
+        if (rootComponent instanceof Container) {
+            for (Component child : ((Container) rootComponent).getComponents()) {
+                changeFontRecursive(child, font);
             }
         }
     }
 
     public static int getFontHeight(Graphics g, Font font) {
-        FontMetrics m= g.getFontMetrics(font); // g is your current Graphics object
-        float totalSize= (float) (font.getSize() * (m.getAscent() + m.getDescent())) / m.getAscent();
+        FontMetrics m = g.getFontMetrics(font); // g is your current Graphics object
+        float totalSize = (float) (font.getSize() * (m.getAscent() + m.getDescent())) / m.getAscent();
         return Math.round(totalSize);
     }
 }
