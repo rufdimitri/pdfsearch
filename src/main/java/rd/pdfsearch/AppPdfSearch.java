3dc686eb1474e7a8a8adfbc1c6e522b892500641
rufdimitri <ruf.dimitri.de@gmail.com>
2024-01-30 14:51:50 +0100

    remove old comment

--- src/main/java/rd/pdfsearch/AppPdfSearch.java
+++ src/main/java/rd/pdfsearch/AppPdfSearch.java
@@ -4,6 +4,5 @@ public class AppPdfSearch {
 
     public static void main(String[] args) {
         new MainWindow("PDF Search", 900, 600);
-        //PDFUtil.searchPdf("C:\\!work\\TestPDFs\\test1.pdf");
     }
 }
